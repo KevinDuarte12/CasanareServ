6c84fedc6067249d438590612e872cc44744b169
KevinDuarte12 <dkevinsantiago2003@gmail.com>
2024-08-19 15:40:43 -0500

    nuevos cambios en CasanareServ

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LogginComponent } from './loggin/loggin.component';
+import { PreguntasFrecComponent } from './preguntas-frec/preguntas-frec.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: HomeComponent},
+    {path: "loggin", component: LogginComponent},
+    {path:"preguntas_frecuentes", component: PreguntasFrecComponent }
+];
