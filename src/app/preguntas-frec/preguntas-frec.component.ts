8e1caf8c0ad74458aa5707fa7f1c8c42c7ab28c9
KevinDuarte12 <dkevinsantiago2003@gmail.com>
2024-08-19 15:22:14 -0500

    Se agrega proyecto

---
+++ src/app/preguntas-frec/preguntas-frec.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preguntas-frec',
+  standalone: true,
+  imports: [],
+  templateUrl: './preguntas-frec.component.html',
+  styleUrl: './preguntas-frec.component.css'
+})
+export class PreguntasFrecComponent {
+
+}
