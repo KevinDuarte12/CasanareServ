3f7da777e6b0b8e7abbda565f84f71461001adf4
KevinDuarte12 <100857261+KevinDuarte12@users.noreply.github.com>
2025-02-17 21:19:56 -0500

    se creo el componente sign-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { CarritoComponent } from './carrito/carrito.component';
+import { WishListComponent } from './wish-list/wish-list.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, LoginComponent, CarritoComponent],
+  imports: [RouterOutlet, HomeComponent, LoginComponent, CarritoComponent, WishListComponent,SignUpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
