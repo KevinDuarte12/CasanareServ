30a48f3635d5e070ef7c7394ade765053d854859
DuvanAcosta007 <duvana99@outlook.com>
2025-02-19 20:45:31 -0500

    carruzel reciclado

--- src/app/carrito/carrito-caruzel.js
+++ src/app/carrito/carrito-caruzel.js
@@ -1,22 +1,13 @@
 let index = 0;
-
 function moveCarousel(direction) {
-    console.log("Ejecutando moveCarousel con dirección:", direction);
-
     const carousel = document.getElementById('carousel');
     if (!carousel) {
         console.error("Error: No se encontró el elemento con ID 'carousel'");
         return;
     }
-
     const items = document.querySelectorAll('.recommended-item');
     const totalItems = items.length;
-
     index = (index + direction + totalItems) % totalItems;
     carousel.style.transform = `translateX(${-index * 100}%)`;
-
-    console.log("Nuevo índice:", index);
 }
-
-// Movimiento automático cada 3 segundos
 setInterval(() => moveCarousel(1), 3000);
