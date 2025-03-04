5ad68d33c03d88ac3590ebae2e8f986c3af4f5f0
DuvanAcosta007 <duvana99@outlook.com>
2025-03-04 06:55:10 -0500

    Update formulario-vender.component.ts

--- src/app/formulario-vender/formulario-vender.component.ts
+++ src/app/formulario-vender/formulario-vender.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {  Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import{ FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
