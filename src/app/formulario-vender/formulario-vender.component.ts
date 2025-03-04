c1a4f92878e19f1287460b9e106cbe63a1d3c935
KevinDuarte12 <100857261+KevinDuarte12@users.noreply.github.com>
2025-03-03 19:32:02 -0500

    se agregaron los componentes requeridos

--- src/app/formulario-vender/formulario-vender.component.ts
+++ src/app/formulario-vender/formulario-vender.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import{ FooterComponent } from '../footer/footer.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-formulario-vender',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, NavbarComponent],
   templateUrl: './formulario-vender.component.html',
   styleUrl: './formulario-vender.component.css'
 })
