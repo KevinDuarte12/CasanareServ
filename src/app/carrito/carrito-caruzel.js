5908b95aeb2c59a0c9fac5c43ca9ea6732f5d720
DuvanAcosta007 <duvana99@outlook.com>
2025-03-16 10:54:00 -0500

    ee

--- src/app/carrito/carrito-caruzel.js
+++ src/app/carrito/carrito-caruzel.js
@@ -1,18 +1,21 @@
 let index = 0;
+
 function moveCarousel(direction) {
     const carousel = document.getElementById('carousel');
-    const item = document.querySelectorAll(".recommended-item");
-    if (!carousel || items.length === 0)  {
+    const items = document.querySelectorAll(".recommended-item"); // Cambio en la variable
+
+    if (!carousel || items.length === 0) {
         console.error("Error: No se encontraron los elementos necesarios");
         return;
     }
-    const itemwidth = items[0].offsetwidth + 20; //Ancho del item + margen//
+
+    const itemWidth = items[0].offsetWidth + 20; // Corregido offsetWidth y renombrado
     const totalItems = items.length;
 
-    index = (index+ direction + totalItems) % totalItems;
+    index = (index + direction + totalItems) % totalItems;
     carousel.style.transform = `translateX(${-index * itemWidth}px)`;
-  }
+}
 
-  document.addEventListener("DOMContentLoaded", function(){
-    setInterval (()=> moveCarousel(1),3000);
-  });
+document.addEventListener("DOMContentLoaded", function () {
+    setInterval(() => moveCarousel(1), 3000);
+});
