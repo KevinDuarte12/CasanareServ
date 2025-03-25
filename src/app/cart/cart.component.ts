04cb01a9beffbed8fc4b9b5ffaf605f26135f7c0
KevinDuarte12 <100857261+KevinDuarte12@users.noreply.github.com>
2025-03-25 17:15:36 -0500

    hola

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from '../header/header.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { BreadcrumbComponent } from '../breadcrumb/breadcrumb.component';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
-  imports: [HeaderComponent, NavbarComponent, BreadcrumbComponent, FooterComponent],
+  imports: [HeaderComponent, NavbarComponent, BreadcrumbComponent, FooterComponent, RouterLink],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
