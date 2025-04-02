5fdba4f591b4c639dade3bf230dbfbb47b7253c5
DuvanAcosta007 <duvana99@outlook.com>
2025-04-01 23:50:57 -0500

    ca

--- CasanareServF/src/app/cart/cart.component.ts
+++ CasanareServF/src/app/cart/cart.component.ts
@@ -16,15 +16,16 @@ export class CartComponent implements OnInit, OnDestroy{
   @ViewChild('slidesContainer') slidesContainer!: ElementRef;
 
   slides = [
-    { image: 'img/mano_libres.jpg', alt: 'Slide 1',  },
-    { image: 'img/zapatilla.jpg', alt: 'Slide 2' },
-    { image: 'img/mouse.jpg', alt: 'Slide 3' },
-    { image: 'img/relog.jpg', alt: 'Slide 4' },
-    { image: 'img/vendor-5.jpg', alt: 'Slide 5' },
-    { image: 'img/vendor-6.jpg', alt: 'Slide 6' },
-    { image: 'img/vendor-7.jpg', alt: 'Slide 7' },
-    { image: 'img/vendor-8.jpg', alt: 'Slide 8' }
+    { image: 'img/mano_libres.jpg', alt: 'Slide 1', name: 'Auriculares Bluetooth', price: '$50', quantity: 10 },
+    { image: 'img/zapatilla.jpg', alt: 'Slide 2', name: 'Zapatillas deportivas', price: '$70', quantity: 5 },
+    { image: 'img/mouse.jpg', alt: 'Slide 3', name: 'Mouse inalámbrico', price: '$25', quantity: 15 },
+    { image: 'img/relog.jpg', alt: 'Slide 4', name: 'Reloj inteligente', price: '$120', quantity: 8 },
+    { image: 'img/vendor-5.jpg', alt: 'Slide 5', name: 'Cámara de seguridad', price: '$200', quantity: 3 },
+    { image: 'img/vendor-6.jpg', alt: 'Slide 6', name: 'Teclado mecánico', price: '$90', quantity: 7 },
+    { image: 'img/vendor-7.jpg', alt: 'Slide 7', name: 'Monitor 24 pulgadas', price: '$180', quantity: 4 },
+    { image: 'img/vendor-8.jpg', alt: 'Slide 8', name: 'Altavoz portátil', price: '$60', quantity: 12 }
   ];
+
 
   currentIndex = 0;
   slidesPerView = 4;
